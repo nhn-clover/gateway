a4ab40f4d3fedc2abb0fcb81cf8e099f3cbc7e1b
조승주 <tmdwn3318@gmail.com>
2024-11-29 08:57:45 +0900

    회원가입 페이지 생성 로그인 html에 회원가입 링크 생성 web config에서 회원가입 뷰 등록 MemberRegisterController 생성 register.html 생성 member id를 memberId로 변경

---
+++ src/main/java/com/nhnacademy/gateway/member/controller/MemberRegisterController.java
@@ -0,0 +1,27 @@
+package com.nhnacademy.gateway.member.controller;
+
+import com.nhnacademy.gateway.member.domain.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/auth/register")
+public class MemberRegisterController {
+
+    @PostMapping
+    public String register(
+        @RequestParam("id") String id,
+        @RequestParam("password") String password,
+        @RequestParam("email") String email
+    ) {
+        Member member = new Member(id, password, email);
+
+        // TODO 어카운트 서버로 멤버 등록 요청 전송
+
+        // 등록 처리 후 로그인으로 다시 이동
+        return "redirect:/auth/login";
+    }
+
+}
--- src/main/java/com/nhnacademy/gateway/member/domain/Member.java
+++ src/main/java/com/nhnacademy/gateway/member/domain/Member.java
@@ -7,8 +7,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 public class Member {
-    private long memberId;
-    private String id;
+    private long id;
+    private String memberId;
     private String name;
     private String email;
+
+    public Member(String memberId, String name, String email) {
+        this.memberId = memberId;
+        this.name = name;
+        this.email = email;
+    }
 }
