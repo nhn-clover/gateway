ea9a6d8f00d5410a11c8510fd5f5c25c6d24e586
조승주 <tmdwn3318@gmail.com>
2024-11-28 14:23:32 +0900

    get members/{id} 좀더 추가

--- src/main/java/com/nhnacademy/gateway/member/controller/MemberController.java
+++ src/main/java/com/nhnacademy/gateway/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.gateway.member.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/members")
 public class MemberController {
 
-    @GetMapping("/{id}")
+    @GetMapping("/myprojects")
     public String view(
+//            @AuthenticationPrincipal Member member,
             @PathVariable String id,
             Model model
     ) {
-        // TODO id의 멤버 정보를 서비스에서 가져오거나 페인클라이언트로 어카운트 서버에서 받아와야함
+
+        // TODO 멤버 컨트롤러
+        // id의 멤버 정보를 서비스에서 가져오거나 페인클라이언트로 어카운트 서버에서 받아와야함
+        
+
+        // 자신의 아이디로 프로젝트 리스트를 받아와서 모델에 넘겨주기
+//        model.addAttribute("projectList", projectList);
+
         return "member/memberView";
     }
+
 }
