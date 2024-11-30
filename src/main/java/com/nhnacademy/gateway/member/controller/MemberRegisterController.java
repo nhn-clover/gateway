cf699d2da54d95c0f11efbe84b4f0b6d5e21e03f
조승주 <tmdwn3318@gmail.com>
2024-11-30 14:26:09 +0900

    security config 에서 /auth/register 접근 모두 가능하도록 추가 webconfig에 get register 추가해봤는데 안돼서 컨트롤러에서 직접 등록함

--- src/main/java/com/nhnacademy/gateway/member/controller/MemberRegisterController.java
+++ src/main/java/com/nhnacademy/gateway/member/controller/MemberRegisterController.java
@@ -2,6 +2,7 @@ package com.nhnacademy.gateway.member.controller;
 
 import com.nhnacademy.gateway.member.domain.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/auth/register")
 public class MemberRegisterController {
+
+    @GetMapping
+    public String register() {
+        return "/auth/register";
+    }
 
     @PostMapping
     public String register(
