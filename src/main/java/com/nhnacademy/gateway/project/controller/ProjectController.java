215bc6dd1d3040e5ac19b71b42420e4192472ed1
gyeongmin2121@gmail.com <gyeongmin2121@gmail.com>
2024-12-01 17:51:04 +0900

    project page 생성중

--- src/main/java/com/nhnacademy/gateway/project/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/gateway/project/controller/ProjectController.java
@@ -1,22 +1,41 @@
 package com.nhnacademy.gateway.project.controller;
 
+import com.nhnacademy.gateway.project.domain.Project;
+import com.nhnacademy.gateway.project.domain.Status;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Slf4j
+@RequestMapping("/projects")
 public class ProjectController {
 
-    @GetMapping("/projects")
-    public String createProjectView(){
-        return "project/createProject";
+    @GetMapping
+    public String viewListProject(Model model){
+
+        List<Project> projects = new ArrayList<>();
+
+        projects.add(new Project(1L , "프로젝트1", Status.IN_PROGRESS));
+        projects.add(new Project(2L , "프로젝트2", Status.IN_PROGRESS));
+        projects.add(new Project(3L , "프로젝트3", Status.IN_PROGRESS));
+
+
+        model.addAttribute("projects", projects);
+
+        return "/project/projectList";
     }
+
+//    @GetMapping
+//    public String createProjectView(){
+//        return "project/createProject";
+//    }
     @PostMapping("/projects")
     public String createProject(@AuthenticationPrincipal UserDetails userDetails,
                                 @RequestParam("projectName") String  projectName){
@@ -28,12 +47,28 @@ public class ProjectController {
         return "home";
     }
 
-    @GetMapping("/projects/{projectId}")
-    public String viewDetailProject(@PathVariable("projectId") long projectId){
+    @GetMapping("/{projectId}")
+    public String viewDetailProject(@PathVariable("projectId") long projectId, Model model){
         log.info("projectId : {}", projectId);
+
+        List<String> tasks = List.of("테스크1", "테스크2","테스크4", "테스크5");
+
+
+        model.addAttribute("tasks", tasks);
 
         return "/project/projectDetail";
     }
+
+
+
+
+//    @GetMapping("/{projectId}/detail")
+//    public String getProjectDetail(@PathVariable long projectId, Model model) {
+//
+//        return "projectDetail"
+//    }
+
+
 
 
 }
