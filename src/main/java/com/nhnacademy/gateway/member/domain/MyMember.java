c50225d9b53e6b07d35ce3d1d75326fec04e9e68
조승주 <tmdwn3318@gmail.com>
2024-12-01 15:31:31 +0900

    pom 에서 오픈 페인클라 버전 명시함 MyMember jpa entity 연관된 거 없앰 (db 테이블처럼 안쓸 것이기 때문)

--- src/main/java/com/nhnacademy/gateway/member/domain/MyMember.java
+++ src/main/java/com/nhnacademy/gateway/member/domain/MyMember.java
@@ -1,10 +1,5 @@
 package com.nhnacademy.gateway.member.domain;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,8 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MyMember {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long myMemberId;
     String name;
     String id;
