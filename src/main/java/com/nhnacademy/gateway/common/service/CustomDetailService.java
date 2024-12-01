d1c3b2d17cfe6dc58e949c2f1fa3e11a1dd3a608
bbangho <dudgh614@gmail.com>
2024-12-01 21:41:45 +0900

    detailsservice connect to account server

--- src/main/java/com/nhnacademy/gateway/common/service/CustomDetailService.java
+++ src/main/java/com/nhnacademy/gateway/common/service/CustomDetailService.java
@@ -1,12 +1,17 @@
 package com.nhnacademy.gateway.common.service;
 
+import com.nhnacademy.gateway.common.client.AccountApiClient;
+import com.nhnacademy.gateway.member.domain.Member;
 import com.nhnacademy.gateway.member.domain.MyMember;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -16,24 +21,27 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Profile("team")
+@Slf4j
 public class CustomDetailService implements UserDetailsService {
     // feign client
     // Member 가져오는 일을 한 후 ?
     // Academy User
     private final RedisTemplate<String, Object> redisTemplate;
+    private final AccountApiClient accountApiClient;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // TODO Redis -> feign client로 바꿔서
-        // redis Member -> username -> Member 객체를 받아서
-//        Optional<MyMember> myMemberById = memberRepository.findMyMemberById(username);
 
-//
-//        if(myMemberById.isPresent()){
-//            MyMember myMember = myMemberById.get();
-//            return new AuthenticatedMember(myMember.getId(), myMember.getPassword());
-//        }
+        log.info("call load User By Username");
+        Member member = accountApiClient.getMember(username);
+        log.info("member : {}", member);
 
-        throw new RuntimeException();
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        boolean matches = passwordEncoder.matches("test", member.getPassword());
+        log.info("matches :{}", matches);
+
+
+        return new AuthenticatedMember(member.getMemberId(),member.getPassword() );
     }
 }
--- src/main/java/com/nhnacademy/gateway/member/domain/Member.java
+++ src/main/java/com/nhnacademy/gateway/member/domain/Member.java
@@ -1,23 +1,23 @@
 package com.nhnacademy.gateway.member.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Member {
     private long id;
     private String memberId;
     private String name;
+    private String password;
     private String email;
     private Status status;
 
-    public Member(String memberId, String name, String email, Status status) {
+    public Member(String memberId, String password,String name, String email, Status status) {
         this.memberId = memberId;
+        this.password = password;
         this.name = name;
         this.email = email;
         this.status = status;
